95935190fa5043898b52e4d51df3b0e1333d22d5
Mauro Capu <maurocapu@hotmail.com>
2022-04-05 11:21:50 -0300

    Achicando el codigo con funcion

--- conversor_de_moneda.py
+++ conversor_de_moneda.py
@@ -1,40 +1,30 @@
-#Valores
-usuario = input("Introduce tu nombre de Usuario: ")
-dinero = float(input("Introduce el dinero a convertir: "))
-eth = 3493.47
-dolar = 111.34
-euro = 122.84
-btc = 46390.30
-#Calculo a dolar
-dolares = dinero / dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-#Calculo a euro
-euros = dinero / euro
-euros = round(euros, 2)
-euros = str(euros)
-#Calculo a eth
-eths = dinero / eth
-eths = round(eths, 2)
-eths = str(eths)
-#Calculo a BTC
-btcs = dinero / btc
-btcs = round(btcs, 2)
-btcs = str(btcs)
-#Input del Usuario
-print("1.Dolar")
-print("2.Euro")
-print("3.ETH")
-print("4.BTC")
-moneda = input("Introduce a la moneda que deseas convertir: ")
-moneda = int(moneda)
-if moneda == 1:
-    print(usuario + " Tienes $"+ dolares + " dolares")
-elif moneda == 2:
-    print(usuario + " Tienes €"+ euros + " euros")
-elif moneda == 3:
-    print(usuario + " Tienes $"+ eths + " ethereum")
-elif moneda == 4:
-    print(usuario + " Tienes $"+ btcs + " bitcoins")
+usuario = input("Introduce tu nombre de usuario: ")
+def convert(tipo_moneda, valor_moneda):
+    dinero = int(input("¿Cuanto dinero desea convertir?: "))
+    moneda = dinero / valor_moneda
+    moneda = round(moneda, 2)
+    moneda = str(moneda) 
+    print(usuario + ", tienes $" + moneda, tipo_moneda)
+
+menu = """
+Bienvenido al conversor de monedas """ + usuario + """
+
+1 - Dolar
+2 - Euro
+3 - Ethereum
+4 - Bitcoin
+
+Elige una opcion: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    convert("dolares", 111.34)
+elif opcion == 2:
+    convert("euros", 122.84)
+elif opcion == 3:
+    convert("ethereum", 388962.94)
+elif opcion == 4:
+    convert("bitcoins", 5165096.0)
 else:
-    print("La opcion ingresada no es valida, vuelva a intentarlo.")
+    print("Ingresa una opcion valida por favor")
